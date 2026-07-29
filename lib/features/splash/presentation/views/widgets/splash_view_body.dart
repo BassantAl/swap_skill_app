3f7c632be61525180cd9504db1afe298f19e0066
bassantAli <basant.ali123zx@gmail.com>
2026-07-29 14:38:33 +0300

    chage ui for splash view make it responsive

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,34 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:swap_skill/core/constants/assets.dart';
 import 'package:swap_skill/core/theme/app_styles.dart';
-import 'package:swap_skill/core/widgets/custom_loading_indicator.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(child: SizedBox()),
-         Center(
-          child: Image.asset(Assets.imagesAppLogo2, width: 120, height: 120),
-        ),
-        const SizedBox(height: 24),
-         Container(
-          padding:const EdgeInsets.symmetric(horizontal: 16),
-          decoration: BoxDecoration(color: const Color(0xffFFFFFF)),
-          child: Text('Swap Skill', style: AppStyles.bold38(context)),
-        ),
-        const SizedBox(height: 8),
-         Text('Learn. Teach. Grow Together.', style: AppStyles.medium20(context)),
-
-         Expanded(
-           child: CustomLoadingIndicator(),
-         ),
+    return SingleChildScrollView(
+      child: ConstrainedBox(
         
-      ],
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.sizeOf(context).height
+        ),
+        child: IntrinsicHeight(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             Spacer(),
+               Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(Assets.imagesAppLogo2, width: 120, height: 120)),
+              ),
+              Spacer(),
+               FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text('Learn. Teach. Grow Together.', style: AppStyles.medium20(context))),
+          SizedBox(height: 60,)
+              
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
